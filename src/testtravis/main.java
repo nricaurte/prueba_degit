f8476ec41bec065cba2b835cb7ea76eaa5ce44ea
nricaurte <fernando.ricaurte@hotmail.com>
2015-05-18 21:25:25 +0200

    Subiendo proyecto a github

---
+++ src/testtravis/main.java
@@ -0,0 +1,33 @@
+package testtravis;
+
+import javax.swing.JOptionPane;
+
+public class main {
+
+	public static void main(String[] args) {
+		  int a;
+	      Operaciones s=new Operaciones();
+	        a = Integer.parseInt(JOptionPane.showInputDialog("Elija Opcion "
+	                + " \n 1.Sumar"
+	                + " \n 2.restar"
+	                + " \n 3.multiplicar"
+	                +"  \n4.Dividir"));
+	        switch(a){
+	            case 1:
+	               JOptionPane.showMessageDialog(null, s.sumar(3,3));
+	            break;
+	            case 2:
+	                JOptionPane.showMessageDialog(null, s.restar(3,3));
+	            break;
+	            case 3:
+	                JOptionPane.showMessageDialog(null, s.multiplicar(3,3));
+	            break;
+	            case 4:
+	                JOptionPane.showMessageDialog(null, s.dividir(3,3));
+	            break;
+	        
+	        }
+
+	}
+
+}
